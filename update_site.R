021a394c557f489a52c004417d77a8bfb90bab63
yourname <yourname@email.com>
2026-04-17 15:08:03 +0200

    update 2026-04-17 15:08:03.144793

--- update_site.R
+++ update_site.R
@@ -1,6 +1,6 @@
 get_data <- function(){
   # https://www.obrien.page/blog/2023/03_10_google_and_github_actions/
-  if(!Sys.info()["user"] == "vanlissa") googlesheets4::gs4_auth(path = Sys.getenv('GOOGLE_DRIVE'))
+  #if(!Sys.info()["user"] == "vanlissa") googlesheets4::gs4_auth(path = Sys.getenv('GOOGLE_DRIVE'))
   websitedata <- googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/1ii_T5tXEp1CzUWivy4f0QFFkQcoKhefxqX2UmpuvELo/edit?usp=sharing')
   names(websitedata) <- c("Timestamp", "category", "title", "description", "date", "enddate", "removeby", "link", "picture")
   websitedata[c("date", "enddate", "removeby")] <- lapply(websitedata[c("date", "enddate", "removeby")], as.Date, format = "%d-%m-%Y")
